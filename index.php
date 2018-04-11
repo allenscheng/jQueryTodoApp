36f8bd1b8a9c98b8f2abfaaee923c623110623a4
Allen Cheng <allencheng219@gmail.com>
2018-04-11 02:17:47 -0500

    changed jQuery to CDN instead of src locally

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <script type="text/javascript" src="assets/js/lib/jquery-3.3.1.min.js"></script>
+  <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="assets/css/todoApp.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
